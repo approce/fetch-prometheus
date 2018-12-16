eb6218af05a370019fa41536edf224aa4c1103a7
user <s>
2018-12-16 14:28:06 +0200

    Support passing custom fetch module by request options

--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
+const debug                            = require('debug')('fetch-prometheus');
 const {requestCount, requestHistogram} = require('./metrics');
 const {generalizeStatusCode}           = require('./normalizer');
 
-const setup = fetch => {
-  if (!fetch) {
+const setup = (uri, opts) => {
+  let fetch;
+
+  if (opts.fetch && typeof opts.fetch === 'function') {
+    fetch = opts.fetch;
+    debug('using custom fetch module passed in options');
+  } else {
     fetch = require('node-fetch');
+    debug('using default node-fetch module');
   }
 
-  const fetchPrometheus = async (url, opts = {}) => {
-    const method     = opts.method ? opts.method.toUpperCase() : 'GET';
-    const publishURL = opts.publishURL ? opts.publishURL : url;
+  return fetchPrometheusWrapper(fetch, uri, opts);
+};
 
-    let result;
-    const end = requestHistogram.startTimer({url: publishURL, method});
+const fetchPrometheusWrapper = async (fetch, url, opts = {}) => {
+  const method     = opts.method ? opts.method.toUpperCase() : 'GET';
+  const publishURL = opts.publishURL ? opts.publishURL : url;
 
-    try {
-      result = await fetch(url, opts);
-    } finally {
-      const status            = result.status;
-      const generalizedStatus = generalizeStatusCode(status);
+  const end = requestHistogram.startTimer({url: publishURL, method});
+  let result;
 
-      end({generalized_status: generalizedStatus, status});
-      requestCount.inc({url: publishURL, method, generalized_status: generalizedStatus, status});
-    }
+  try {
+    result = await fetch(url, opts);
+  } finally {
+    const status            = result.status;
+    const generalizedStatus = generalizeStatusCode(status);
 
-    return result;
-  };
+    end({generalized_status: generalizedStatus, status});
+    requestCount.inc({url: publishURL, method, generalized_status: generalizedStatus, status});
 
-  return fetchPrometheus;
+    debug(`Status: ${status}. Method: ${method}. URL: ${publishURL}`)
+  }
+
+  return result;
 };
 
 module.exports = setup;
